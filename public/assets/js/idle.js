b6239ffe4f3539da66c816dd7b9c586bd8e426e7
Weng Fei Fung <weffung@ucdavis.edu>
2021-01-29 23:26:19 -0800

    Idling-logout system: Warns you at 1 minute idling, Logs you out at 2 minutes idling

---
+++ public/assets/js/idle.js
@@ -0,0 +1,19 @@
+idle({
+    idle: 60000,
+    onIdle: function() {
+        let isLoggedIn = $(".loggedInUser").length;
+        if (isLoggedIn)
+            alert("You will be logged out for inactivity after 1 minute.");
+    }
+}).start();
+
+idle({
+    idle: 120000,
+    onIdle: function() {
+        let isLoggedIn = $(".loggedInUser").length;
+        if (isLoggedIn) {
+            alert("Logging out for inactivity...");
+            document.location.pathname = "/logout";
+        }
+    }
+}).start();
